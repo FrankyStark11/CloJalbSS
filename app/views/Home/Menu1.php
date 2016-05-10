eb0ae9df0b2b6919a0c46d715867538485f861f0
Francis Marsolais <francis.marsolaisv4si@gmail.com>
2016-05-10 15:07:08 -0400

    MODIFICATION DES lien et page web

--- app/views/Home/Menu1.php
+++ app/views/Home/Menu1.php
@@ -47,7 +47,7 @@
 
 					<div class="infobox boxarticle" align="center"><a href="/index.php/Aide/Accueil"><img title="J'ai une question sur le système intranet" src="../../images/icon/aide-icon.png"></a><h2>AIDE</h2></div>
 
-					<div class="infobox boxarticle" align="center"><a href="/SousConstructionHTML/index.html"><img title="SITE WEB CLOTURE JALBERT" src="../../images/icon/WEB-icon.png"></a><h2>WEB</h2></div>
+					<div class="infobox boxarticle" align="center"><a href="www.cloturejalbert.com"><img title="SITE WEB CLOTURE JALBERT" src="../../images/icon/WEB-icon.png"></a><h2>WEB</h2></div>
 				</div>
 			</div>
 			<div class="col-2 col-m-12"></div>
