ee7ca41a47e867780c2850cbf848f80a16c43a02
Francis Marsolais <francis.marsolaisv4si@gmail.com>
2016-05-20 09:19:26 -0400

    fin de stage visual du login update

--- app/views/Home/Login.php
+++ app/views/Home/Login.php
@@ -3,6 +3,8 @@
 <head>
 
 	<title>Soumission Clôture Jalbert</title>
+	<script type="text/javascript" src="/js/jquery-1.12.1.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="/css/style2.css" />
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
@@ -13,64 +15,51 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<link rel="stylesheet" type="text/css" href="/css/style2.css" />
-	<script type="text/javascript" src="/js/jquery-1.12.1.min.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	
+	
 	<script type="text/javascript" src="/js/javascript.js"></script>
-	<script src="https://use.fonticons.com/48fddf7d.js"></script>
+	<script src="https://use.fontawesome.com/bf0a763563.js"></script>
 
 
 </head>
 
-<body>
+<body style="background-color: #3F4F5A;">
 
 <div class="row">
 
-<div class="col-12 col-l-12 col-m-12" align="center">
-	<div class="col-12 col-m-12 box bgorange">
-		<h2>INTRANET JALBERT</h2>
-	</div>
-</div>
-
-<div class="col-3 col-m-12 middle" align="center">
-	<div class="col-12 col-m-12 ">
-	</div>
-</div>
-
-<div class="col-6 col-m-12 middle" align="center">
-	
-	<div class="col-12 col-m-12 box">
-		<form method="post" action="/index.php/Admin/Login">
-
-		<div class="input-group">
-		  <span class="input-group-addon  glyphicon glyphicon-user" id="sizing-addon2"></span>
-		  <input type="text" class="form-control" name="User" placeholder=" Nom d'utilisateur " aria-describedby="sizing-addon2">
+	<div class="col-12 col-l-12 col-m-12" align="center">
+		<div class="col-12 col-m-12 box bgorange">
+			<h2><i class="fa fa-1x fa-spin fa-compass" aria-hidden="true"></i> INTRANET JALBERT </h2>
 		</div>
+	</div>
+</div>
+<div >
+	<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4 middle" >
+		<div class="box">
+			<form method="post" action="/index.php/Admin/Login">
 
-		<div class="input-group">
-		  <span class="input-group-addon glyphicon glyphicon-lock" id="sizing-addon2"></span>
-		  <input type="password" class="form-control" name="MDP" placeholder=" Mot de passe " aria-describedby="sizing-addon2">
+				<div class="form-group has-feedback">
+					<label for="exampleInputEmail1">Nom d'utilisateur</label>
+				  	<input type="text" class="form-control" name="User" placeholder=" Nom d'utilisateur " aria-describedby="sizing-addon2">
+				  	<span class="glyphicon glyphicon-user form-control-feedback" aria-hidden="true"></span>
+		  			<span id="inputSuccess2Status" class="sr-only">(success)</span>
+				</div>
+
+				<div class="form-group has-feedback">
+					<label for="exampleInputEmail1">Mot de passe</label>
+				  	<input type="password" class="form-control" name="MDP" placeholder=" ********** " aria-describedby="sizing-addon2">
+				  	<span class="glyphicon glyphicon-lock form-control-feedback" aria-hidden="true"></span>
+		  			<span id="inputSuccess2Status" class="sr-only">(success)</span>
+				</div>
+
+				<hr>
+
+				<button type="submit" class="btn btn-success btn-lg btn-block">CONNEXION</button>
+
+			</form>	
 		</div>
-
-		<hr>
-
-		<button type="submit" class="btn btn-success btn-lg btn-block">
-				<span class="glyphicon glyphicon-ok" aria-hidden="true"></span> CONNEXION
-		</button>
-
-		</form>	
 	</div>
-	
-</div>
-
-
-<div class="col-3 col-m-12 middle" align="center">
-	<div class="col-12 col-m-12 ">
-		
-	</div>
-</div>
-
-
 </div>
 </body>
 
